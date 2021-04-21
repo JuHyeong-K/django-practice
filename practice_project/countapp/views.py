786246217ef1cde492eb80028e161c4826e5b1e5
JuHyeong <wngud4680@gmail.com>
2021-04-21 15:38:07 +0900

    feat: Add text_count function
    TODO
    - Add counting text with space function
    - Add counting text with no-space function

--- practice_project/countapp/views.py
+++ practice_project/countapp/views.py
@@ -5,4 +5,15 @@ def index(request):
     return render(request, 'index.html')
 
 def count(request):
-    return render(request, 'count.html')
+    if request.method == 'POST':
+        text = request.POST['text_string']
+        text_length = len(text)
+        text_length_no_space = len(''.join(text.strip().split(' ')))
+        
+        print(text_length, text_length_no_space)
+        context = {
+            'text': text,
+            'length': text_length,
+            'length_no_space': text_length_no_space,
+        }
+    return render(request, 'count.html', context)
