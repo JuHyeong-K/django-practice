2fd9a758ab33418fe2db3117a58c0a5a715d5006
JuHyeong <wngud4680@gmail.com>
2021-04-23 14:02:37 +0900

    feat: Add models.py
    TODO
    - Add table in models.py
    - makemigrations
    - migrate

--- model_practice/crpractice/models.py
+++ model_practice/crpractice/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class MyClass(models.Model):
+    class_num = models.IntegerField()
+    lecturer = models.CharField(max_length=30)
+    class_room = models.CharField(max_length=30)
+    strudents_num = models.IntegerField()
+
+class MyStudents(models.Model):
+    name = models.CharField(max_length=30)
+    phone_num = models.CharField(max_length=30)
+    intro_text = models.TextField()
